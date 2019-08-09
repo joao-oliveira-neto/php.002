ff1fd8bbf4fe7986f44b663bdb63086b93992851
joao-oliveira-neto <joaodabete@gmail.com>
2019-08-08 21:51:53 -0300

    criando cadastro

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <div class="card card-body bg-dark">
+        <h3 class="text-white">Projeto de Controle de Clientes</h3>
+        <p class="text-white">
+            Sistema desenvolvido em PHP e MySQL
+        </p>
+    </div>
+    <br>
+</head>
+<body>
+    
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,11 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>WELL.com</title>
 </head>
+
+
 <body>
-   <h2>OLÁ MUndo</h2>
+  <div class="card card-body bg-dark">
+        <h3 class="text-white">Projeto de Controle de Clientes</h3>
+        <p class="text-white">
+            Sistema desenvolvido em PHP e MySQL
+        </p>
+    </div>
+    <br>
+    <div class="container">
+        <h2>Painel do Sistema</h2>
+        <hr>
+        <h4>Menu</h4>
+        <nav>
+            <ul>
+                <li><a href="cadastrar.php">Cadastro de Clientes</a></li>
+                <li><a href="consultar.php">Consulta de Clientes</a></li>
+            </ul>
+        </nav>
+    </div>
+   
     
 </body>
 </html>
