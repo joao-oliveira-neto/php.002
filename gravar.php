d51e879960d45ca19c8d66352611ce82b17f10da
joao-oliveira-neto <47464945+joao-oliveira-neto@users.noreply.github.com>
2019-08-15 21:47:00 -0300

    Add files via upload

--- gravar.php
+++ gravar.php
@@ -4,5 +4,14 @@
     $email = $_POST["email"];
     $estadocivil = $_POST["sexo"];
     $sexo = $_POST["sexo"];
+    $con = mysqli_connect("localhost","root","","well");
+    $sql = "INSERT INTO cliente VALUES(null,'{$nome}','{$email}','{$estadocivil}','{$sexo}')";
+    if(mysqli_query($con, $sql))
+    {
+        echo "gravado com sucesso!!";   
+    }else{
+        echo "erro ao gravar";
+        
+    }
     
 ?>
